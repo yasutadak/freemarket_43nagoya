9d839d751cb6e661aff32242ba345f141e1e3b2a
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-02-01 14:25:59 +0900

    Add column to products

--- db/migrate/20190201051410_create_products.rb
+++ db/migrate/20190201051410_create_products.rb
@@ -1,8 +1,18 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
+      t.string  :name,               null: false, index:true
+      t.integer :status,             null: false
+      t.integer :shipping_method,    null: false
+      t.string  :region,             null: false
+      t.integer :shipping_timetable, null: false
+      t.text    :description,        null: false
+      t.integer :price,              null: false
 
-      t.timestamps
+      t.timestamps null: false
+
+      t.references :seller, null: false, foreign_key: { to_table: :users }
+      t.references :buyer,  null: false, foreign_key: { to_table: :users }
     end
   end
 end
