d228cab5663b4febd7820a667093173a526af1c1
Uuya <a1b23cd@icloud.com>
2019-02-01 19:56:14 +0900

    Set local environment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,13 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
+ if Rails.env.development? || Rails.env.test?
+   config.storage = :file
+ elsif Rails.env.production?
+   config.storage = :fog
+   config.fog_provider = ‘fog/aws’
+   config.fog_credentials = {
+     provider: ‘AWS’,
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
@@ -14,4 +17,5 @@ CarrierWave.configure do |config|
 
   config.fog_directory  = 'image-uploader-43'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/image-uploader-43'
+ end
 end
