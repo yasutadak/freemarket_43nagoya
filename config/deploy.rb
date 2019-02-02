3dc09020c760817c4ed44a99e8508f2287a00687
Takeuchi-Kazuhito <Takeuchi.Kazuhito.01@gmail.com>
2019-02-02 18:18:18 +0900

    master へ一括修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,8 +37,6 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
 end
 
-
-
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
