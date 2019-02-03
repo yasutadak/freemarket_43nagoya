c848732e8dae1b2934407e2a85c1fba6fd836ddc
yasutadak <yasutada629@gmail.com>
2019-02-03 14:03:11 +0900

    add_create-action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,17 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+  end
+
+  private
+
+  def product_params
+    params[:product].require(:product).permit(:name, :status, :shipping_method, :region, :shipping_timetable, :price, :description, :seller_id)
   end
 end
