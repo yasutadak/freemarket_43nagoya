feb1bc65dff02ac74c7d457002688eef6a844659
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-02-05 14:10:23 +0900

    Add page transition to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :seller, class_name: 'User'
   belongs_to :buyer, class_name: 'User', optional: true
+
+  def prev
+    Product.where("id < ?", self.id).order("id DESC").first
+  end
+
+  def next
+    Product.where("id > ?", self.id).order("id ASC").first
+  end
 end
