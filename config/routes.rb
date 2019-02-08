187b3bbf15c4cd0694ef02be6351268e3e241a26
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-02-08 12:12:31 +0900

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :users
-  resources :products do
+  resources :users, only: [:show]
+  resources :products, only: [:index, :new, :show] do
     collection do
       get 'list'
     end
