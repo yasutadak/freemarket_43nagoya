6a3adecb5064a8cbd39ee5925f94e9fb01fee77a
Uuya <a1b23cd@icloud.com>
2019-02-09 12:47:19 +0900

    Add devise validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,37 @@ class User < ApplicationRecord
   has_many :selling_products, -> { where('buyer_id is NULL') }, foreign_key: 'seller_id', class_name: 'Product'
   has_many :sold_products, -> { where('buyer_id is not NULL') }, foreign_key: 'buyer_id', class_name: 'Product'
   has_many :bought_products, foreign_key: 'buyer_id', class_name: 'Product'
+
+  with_options on: [:registration_step_1] do
+    validates :nickname,  presence: true
+  end
+
+  with_options on: [:registration_step_2] do
+    validates :phone_number,  presence: true
+  end
+
+  with_options on: [:registration_step_3] do
+    validates :family_name,  presence: true
+    validates :first_name,  presence: true
+    validates :family_katakana,  presence: true
+    validates :family_katakana, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, allow_blank: true}
+    validates :first_katakana,  presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, allow_blank: true}
+    validates :postal_code,  presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true}
+    validates :domicile_pref,  presence: true
+    validates :domicile_city,  presence: true
+    validates :domicile_add,  presence: true
+  end
+
+  protected
+  def password_required?
+    if validation_context == :registration_step_1
+      !persisted? || !password.nil? || !password_confirmation.nil?
+    end
+  end
+
+  def email_required?
+    if validation_context == :registration_step_1
+      true
+    end
+  end
 end
