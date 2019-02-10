68f8f1712f8eb597590eeb99fb886cc7ab8d942b
Uuya <a1b23cd@icloud.com>
2019-02-10 16:08:14 +0900

    Make  dummy data user

---
+++ spec/factories/users.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define do
+
+  factory :user do
+    nickname              "test"
+    email                 "test@gmail.com"
+    password              "00000000"
+    phone_number          "00000000000"
+    family_name           "てすと"
+    first_name            "てすと"
+    family_katakana       "テスト"
+    first_katakana        "テスト"
+    postal_code           "000-0000"
+    domicile_pref         "愛知県"
+    domicile_city         "テスト市"
+    domicile_add          "100丁目"
+    building              "テストアパート101"
+  end
+
+end
