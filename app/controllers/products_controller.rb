72ec31bccb188105da207a52e9ffd57ed2d57f43
yasutadak <yasutada629@gmail.com>
2019-02-12 17:44:05 +0900

    edit_enum
    表記がおかしかったため

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,12 +21,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
-
-  def product_params
-    params.require(:product).permit(:name, :status, :shipping_method, :shipping_burden, :region, :shipping_timetable, :price, :description, :image, :image_cache).merge(seller_id: 1)
-  end
-
   def edit
     @product = Product.find(params[:id])
   end
@@ -38,4 +32,11 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @products = Product.where(seller_id: @product.seller).where.not(id: @product.id).order("id DESC").limit(6)
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :status, :shipping_method, :shipping_burden, :region, :shipping_timetable, :price, :description, :image, :image_cache).merge(seller_id: 1)
+  end
+
 end
