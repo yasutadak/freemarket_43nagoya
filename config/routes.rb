fec18b744a5dfc064366334fc821b923302ba2ee
Takeuchi-Kazuhito <Takeuchi.Kazuhito.01@gmail.com>
2019-02-12 19:27:35 +0900

    ルーティングを追加しました（ profiles コントローラーの show メソッド） & 不要なルーティングを除外しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :users, only: [:show] do
-    resource :logout
-    resource :identification
-    resource :profile
+  resources :users, only: :show do
+    resource :logout, only: :show
+    resource :identification, only: [:edit, :update]
+    resource :profile, only: [:edit, :update, :show]
   end
   resources :products, only: [:index, :new, :show] do
     collection do
