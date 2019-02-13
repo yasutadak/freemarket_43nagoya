b1ed168997cd7a855c7556695358ffbbd9cae2ec
Takeuchi-Kazuhito <Takeuchi.Kazuhito.01@gmail.com>
2019-02-13 17:36:31 +0900

    show アクションが private に入っていたので、元に戻しました

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,14 +21,16 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+    @products = Product.where(seller_id: @product.seller).where.not(id: @product.id).order("id DESC").limit(6)
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :status, :shipping_method, :shipping_burden, :region, :shipping_timetable, :price, :description, :image, :image_cache).merge(seller_id: 1)
   end
 
-  def show
-    @product = Product.find(params[:id])
-    @products = Product.where(seller_id: @product.seller).where.not(id: @product.id).order("id DESC").limit(6)
-  end
+
 end
