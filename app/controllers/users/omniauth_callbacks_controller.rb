647d6c0f3c341187ae24de7f28a208e297f06ab1
Uuya <a1b23cd@icloud.com>
2019-02-13 19:47:05 +0900

    Fixed callbacks add session function etc

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,45 +2,31 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    callback_from :facebook
+    callback_for :facebook
   end
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
 
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
 
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 
   private
 
-  def callback_from(provider)
+  def callback_for(provider)
     provider = provider.to_s
-
     @user = User.find_for_oauth(request.env['omniauth.auth'])
-
     if @user.persisted?
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-      sign_in_and_redirect @user, event: :authentication
+      session[:sns_user] = request.env['omniauth.auth'].uid
+      # key rename
+      request.env['omniauth.auth'][:info][:nickname] = request.env['omniauth.auth'][:info].delete(:name)
+      session[:user] = request.env['omniauth.auth'][:info]
+      if SnsCredential.find_by(uid: session[:sns_user]).user_id
+        sign_in_and_redirect User.find(@user.user_id), event: :authentication
+
+      else
+        redirect_to snsphone_new_path
+      end
+
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
