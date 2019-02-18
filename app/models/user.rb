c10312ed80a9ff3936863b25b53753ceb91ff1b0
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-02-18 19:13:40 +0900

    Modify

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
          :omniauthable
 
   has_many :selling_products, -> { where('buyer_id is NULL') }, foreign_key: 'seller_id', class_name: 'Product'
-  has_many :sold_products, -> { where('buyer_id is not NULL') }, foreign_key: 'buyer_id', class_name: 'Product'
+  has_many :sold_products, -> { where('buyer_id is not NULL') }, foreign_key: 'seller_id', class_name: 'Product'
   has_many :bought_products, foreign_key: 'buyer_id', class_name: 'Product'
 
   with_options on: :registration_step_1,  presence: true do
