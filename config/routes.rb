c59493253eb518861f41706552e5547c14f4559e
mako-to-f <makoto.fujimoto.0711ex@gmail.com>
2019-02-19 14:21:21 +0900

    Modify

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {
-  registrations:      'users/registrations',
-  sessions:           'users/sessions',
-  omniauth_callbacks: 'users/omniauth_callbacks'
+    registrations:      'users/registrations',
+    sessions:           'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
   devise_scope :user do
